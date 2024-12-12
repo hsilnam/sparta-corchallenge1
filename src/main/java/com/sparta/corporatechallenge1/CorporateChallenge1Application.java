eaca4eb06bc22d6aad939d457521492922719d33
hsilnam <hsilnam3@gmail.com>
2024-12-12 17:42:38 +0900

    init: project

---
+++ src/main/java/com/sparta/corporatechallenge1/CorporateChallenge1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.corporatechallenge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateChallenge1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateChallenge1Application.class, args);
+    }
+
+}
